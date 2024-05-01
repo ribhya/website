b8287d41ddfeeb1d1130984a525be1a2ade33303
Sarvex ☠ Jatasra <sarvex.jatasra@gmail.com>
2024-05-01 12:55:09 +0530

    Updated Hardware and Construction

--- src/data_files/constants.ts
+++ src/data_files/constants.ts
@@ -4,9 +4,9 @@ export const SITE = {
   title: "Ribhya",
   tagline: "Bringing Prudence to FinSecOps",
   description:
-    "Ribhya offers top-tier hardware tools and expert construction services to meet all your project needs. Start exploring and contact our sales team for superior quality and reliability.",
+  "Ribhya is a sophisticated C-Suite Copilot that enhances hybrid cloud cost management through advanced AI analytics. It offers key features like seamless integration and strategic cost-reduction tools, helping c-suite enforce financial policies and make data-driven decisions."
   description_short:
-    "Ribhya offers top-tier hardware tools and expert construction services to meet all your project needs.",
+    "Ribhya is a sophisticated C-Suite Copilot that enhances hybrid cloud cost management through advanced AI analytics.",
   url: "https://ribhya.com",
   author: "Sarvex",
 };
@@ -35,8 +35,8 @@ export const OG = {
   locale: "en_US",
   type: "website",
   url: SITE.url,
-  title: `${SITE.title}: : Hardware Tools & Construction Services`,
+  title: `${SITE.title}: : Master Your Digital Spend: Optimize, Innovate, and Thrive with Ribhya`,
   description:
-    "Equip your projects with Ribhya's top-quality hardware tools and expert construction services. Trusted by industry leaders, Ribhya offers simplicity, affordability, and reliability. Experience the difference with user-centric design and cutting-edge tools. Start exploring now!",
+  "Ribhya is a sophisticated C-Suite Copilot that enhances hybrid cloud cost management through advanced AI analytics. This SaaS platform automates monitoring and optimizes spending across hybrid cloud environments. It offers key features like seamless integration and strategic cost reduction tools, helping c-suite enforce financial policies and make data-driven decisions. Essential for businesses aiming to leverage digital investments efficiently, Ribhya drives operational efficiency and sustainable growth in the digital era.",
   image: ogImageSrc,
 };
