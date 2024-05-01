7554b5c490bc182552da09242e0f0930c371a228
Sarvex ☠ Jatasra <sarvex.jatasra@gmail.com>
2024-05-01 15:04:36 +0530

    yellow to blue

--- src/components/ui/icons/icons.ts
+++ src/components/ui/icons/icons.ts
@@ -74,7 +74,7 @@ export const Icons = {
       },
     ],
     class:
-      "h-6 w-6 flex-shrink-0 text-neutral-700 hs-tab-active:text-orange-400 dark:text-neutral-300 dark:hs-tab-active:text-orange-300 md:h-7 md:w-7",
+      "h-6 w-6 flex-shrink-0 text-neutral-700 hs-tab-active:text-purple-400 dark:text-neutral-300 dark:hs-tab-active:text-purple-300 md:h-7 md:w-7",
     width: 24,
     height: 24,
     viewBox: "0 0 24 24",
@@ -93,7 +93,7 @@ export const Icons = {
         d: "M12 19V5",
       },
     ],
-    class: "h-5 w-5 flex-shrink-0 text-orange-400 dark:text-orange-300",
+    class: "h-5 w-5 flex-shrink-0 text-purple-400 dark:text-purple-300",
     width: 24,
     height: 24,
     viewBox: "0 0 24 24",
@@ -417,8 +417,7 @@ export const Icons = {
         d: "m20.893 13.393-1.135-1.135a2.252 2.252 0 0 1-.421-.585l-1.08-2.16a.414.414 0 0 0-.663-.107.827.827 0 0 1-.812.21l-1.273-.363a.89.89 0 0 0-.738 1.595l.587.39c.59.395.674 1.23.172 1.732l-.2.2c-.212.212-.33.498-.33.796v.41c0 .409-.11.809-.32 1.158l-1.315 2.191a2.11 2.11 0 0 1-1.81 1.025 1.055 1.055 0 0 1-1.055-1.055v-1.172c0-.92-.56-1.747-1.414-2.089l-.655-.261a2.25 2.25 0 0 1-1.383-2.46l.007-.042a2.25 2.25 0 0 1 .29-.787l.09-.15a2.25 2.25 0 0 1 2.37-1.048l1.178.236a1.125 1.125 0 0 0 1.302-.795l.208-.73a1.125 1.125 0 0 0-.578-1.315l-.665-.332-.091.091a2.25 2.25 0 0 1-1.591.659h-.18c-.249 0-.487.1-.662.274a.931.931 0 0 1-1.458-1.137l1.411-2.353a2.25 2.25 0 0 0 .286-.76m11.928 9.869A9 9 0 0 0 8.965 3.525m11.928 9.868A9 9 0 1 1 8.965 3.525",
       },
     ],
-    class:
-        "w-4 h-4 flex-shrink-0",
+    class: "w-4 h-4 flex-shrink-0",
     viewBox: "0 0 24 24",
     fill: "none",
     strokeWidth: "1.5",
