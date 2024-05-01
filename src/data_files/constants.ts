720a4e21382d45cfbc60de9f9b1365b40ac934c2
Sarvex ☠ Jatasra <sarvex.jatasra@gmail.com>
2024-05-01 14:48:13 +0530

    Updated SVG

--- src/data_files/constants.ts
+++ src/data_files/constants.ts
@@ -4,7 +4,7 @@ export const SITE = {
   title: "Ribhya",
   tagline: "Bringing Prudence to FinSecOps",
   description:
-  "Ribhya is a sophisticated C-Suite Copilot that enhances hybrid cloud cost management through advanced AI analytics. It offers key features like seamless integration and strategic cost-reduction tools, helping c-suite enforce financial policies and make data-driven decisions."
+    "Ribhya is a sophisticated C-Suite Copilot that enhances hybrid cloud cost management through advanced AI analytics. It offers key features like seamless integration and strategic cost-reduction tools, helping c-suite enforce financial policies and make data-driven decisions.",
   description_short:
     "Ribhya is a sophisticated C-Suite Copilot that enhances hybrid cloud cost management through advanced AI analytics.",
   url: "https://ribhya.com",
@@ -37,6 +37,6 @@ export const OG = {
   url: SITE.url,
   title: `${SITE.title}: : Master Your Digital Spend: Optimize, Innovate, and Thrive with Ribhya`,
   description:
-  "Ribhya is a sophisticated C-Suite Copilot that enhances hybrid cloud cost management through advanced AI analytics. This SaaS platform automates monitoring and optimizes spending across hybrid cloud environments. It offers key features like seamless integration and strategic cost reduction tools, helping c-suite enforce financial policies and make data-driven decisions. Essential for businesses aiming to leverage digital investments efficiently, Ribhya drives operational efficiency and sustainable growth in the digital era.",
+    "Ribhya is a sophisticated C-Suite Copilot that enhances hybrid cloud cost management through advanced AI analytics. This SaaS platform automates monitoring and optimizes spending across hybrid cloud environments. It offers key features like seamless integration and strategic cost reduction tools, helping c-suite enforce financial policies and make data-driven decisions. Essential for businesses aiming to leverage digital investments efficiently, Ribhya drives operational efficiency and sustainable growth in the digital era.",
   image: ogImageSrc,
 };
